0cfcb3f73564481c7cbdee35fcfe162ddf4ce455
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-11 18:40:01 +0430

    environment config

--- Chatty-Backend/src/setupServer.ts
+++ Chatty-Backend/src/setupServer.ts
@@ -14,6 +14,7 @@ import HTTP_STATUS from "http-status-codes";
 import compression from "compression";
 import http from "http";
 import "express-async-errors";
+import { config } from "./config";
 
 const SERVER_PORT = 5000;
 
@@ -36,16 +37,16 @@ export class ChattyServer {
     app.use(
       cookieSession({
         name: "session",
-        keys: ["test1", "test2"],
+        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: false,
+        secure: config.NODE_ENV !== "development",
       })
     );
     app.use(hpp());
     app.use(helmet());
     app.use(
       cors({
-        origin: "*",
+        origin: config.CLIENT_URL,
         credentials: true,
         optionsSuccessStatus: 200,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -72,7 +73,7 @@ export class ChattyServer {
     }
   }
 
-  // private createSocketIO(httpServer: http.Server): void {}
+  private createSocketIO(httpServer: http.Server): void {}
 
   private startHttpServer(httpServer: http.Server): void {
     httpServer.listen(SERVER_PORT, () => {
