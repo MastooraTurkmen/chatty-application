d6b296331b72e0f7d46a3945bd634c92a805e6ee
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-11 21:07:23 +0430

    global error handler

--- Chatty-Backend/src/setupServer.ts
+++ Chatty-Backend/src/setupServer.ts
@@ -18,6 +18,11 @@ import { config } from "./config";
 import { Server } from "socket.io";
 import { createClient } from "redis";
 import { createAdapter } from "@socket.io/redis-adapter";
+import applicationRoutes from "./routes";
+import {
+  CustomError,
+  IErrorResponse,
+} from "./shared/globals/helpers/error-handler";
 
 const SERVER_PORT = 5000;
 
@@ -63,9 +68,32 @@ export class ChattyServer {
     app.use(urlencoded({ extended: true, limit: "50mb" }));
   }
 
-  private routeMiddleware(app: Application): void {}
+  private routeMiddleware(app: Application): void {
+    applicationRoutes(app);
+  }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all("*", (req: Request, res: Response) => {
+      res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   private async startServer(app: Application): Promise<void> {
     try {
@@ -95,7 +123,7 @@ export class ChattyServer {
 
   private startHttpServer(httpServer: http.Server): void {
     console.log(`Server has started with process ${process.pid}`);
-    
+
     httpServer.listen(SERVER_PORT, () => {
       console.log(`server is listening on PORT: ${SERVER_PORT}`);
     });
