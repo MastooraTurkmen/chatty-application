ff64661c31a80b5589c56350bf2af273baea627c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-13 20:35:48 +0430

    cloudinary upload

--- Chatty-Backend/src/config.ts
+++ Chatty-Backend/src/config.ts
@@ -1,5 +1,6 @@
-import dotenv from "dotenv";
-import bunyan from "bunyan";
+import dotenv from 'dotenv';
+import bunyan from 'bunyan';
+import cloudinary from 'cloudinary';
 
 dotenv.config({});
 
@@ -14,21 +15,27 @@ class Config {
   public PORT: string | undefined;
   public JWT_EXPIRES_IN: string | undefined;
   public REDIS_HOST: string | undefined;
+  public CLOUD_NAME: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_API_SECRET: string | undefined;
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL;
-    this.JWT_TOKEN = process.env.JWT_TOKEN || "1234";
-    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
-    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
-    this.CLIENT_URL = process.env.CLIENT_URL || "";
-    this.JWT_SECRET = process.env.JWT_SECRET || "";
-    this.PORT = process.env.PORT || "5000";
-    this.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "";
-    this.REDIS_HOST = process.env.REDIS_HOST || "";
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.JWT_SECRET = process.env.JWT_SECRET || '';
+    this.PORT = process.env.PORT || '5000';
+    this.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
   }
 
   public createLogger(name: string): bunyan {
-    return bunyan.createLogger({ name, level: "debug" });
+    return bunyan.createLogger({ name, level: 'debug' });
   }
 
   public validateConfig(): void {
@@ -39,6 +46,14 @@ class Config {
         throw new Error(`Configuration ${key} is undefined.`);
       }
     }
+  }
+
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUD_NAME,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET
+    });
   }
 }
 export const config: Config = new Config();
