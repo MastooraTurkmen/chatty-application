2310bd72a0f3d4a6921429b4163119eaf1009ca5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-14 22:04:06 +0430

    sing up

---
+++ Chatty-Backend/src/features/auth/controllers/signup.ts
@@ -0,0 +1,49 @@
+import { ObjectId } from 'mongoose';
+import { Request, Response } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument, ISignUpDate } from '@auth/interfaces/auth.interface';
+import { authService } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { UploadApiResponse } from 'cloudinary';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+    const checkIfUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+    if (checkIfUserExist) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = SignUp.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      avatarColor
+    });
+
+    const result: UploadApiResponse = (await uploads(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError('File Upload: Error occurred. Try again ');
+    }
+  }
+
+  private signupData(data: ISignUpDate): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+      uId,
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as unknown as IAuthDocument;
+  }
+}
