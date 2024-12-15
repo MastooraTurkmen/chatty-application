5b2379039edc4c7988ed2ba1350f861bb46d109e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-15 20:12:01 +0430

    routes

--- Chatty-Backend/src/routes.ts
+++ Chatty-Backend/src/routes.ts
@@ -1,7 +1,12 @@
+import { authRoutes } from '@auth/routes/authRoutes';
 import { Application } from 'express';
 
-export default (_app: Application) => {
-  const routes = () => {};
+const BASE_PATH = '/api/v1';
+
+export default (app: Application) => {
+  const routes = () => {
+    app.use(BASE_PATH, authRoutes.routes());
+  };
 
   routes();
 };
