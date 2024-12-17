14370eed6f7eafc1fc0c1a6b2df0e8e566ca2f70
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-12-17 19:49:57 +0430

    joi validation

--- Chatty-Backend/src/shared/globals/decorators/joi-validation.decorators.ts
+++ Chatty-Backend/src/shared/globals/decorators/joi-validation.decorators.ts
@@ -2,19 +2,20 @@ import { JoiRequestValidationError } from '@global/helpers/error-handler';
 import { Request } from 'express';
 import { ObjectSchema } from 'joi';
 
-type IJoiDecorator = (target: any, key: string, descriptor: PropertyDecorator) => void;
+type IJoiDecorator = (target: any, key: string, descriptor: TypedPropertyDescriptor<any>) => void;
 
 export function joiValidation(schema: ObjectSchema): IJoiDecorator {
-  return (_targets: any, _key: string, descriptor: PropertyDecorator) => {
+  return (_target: any, _key: string, descriptor: TypedPropertyDescriptor<any>) => {
     const originalMethod = descriptor.value;
 
     descriptor.value = async function (...args: any[]) {
       const req: Request = args[0];
-      // validateAsync or validate
+      // Validate using Joi
       const { error } = await Promise.resolve(schema.validate(req.body));
       if (error?.details) {
         throw new JoiRequestValidationError(error.details[0].message);
       }
+      // Call the original method if validation passes
       return originalMethod.apply(this, args);
     };
     return descriptor;
